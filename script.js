d328c39ebfde1bd1d1c5c7c9143bbcb23650e082
Olatoyan <golatoyan@gmail.com>
2023-05-29 12:53:03 +0100

    new commit

--- script.js
+++ script.js
@@ -1,49 +1,132 @@
 "use strict";
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition((position) => {
-    console.log(position);
-    const { latitude } = position.coords;
-    const { longitude } = position.coords;
-    console.log(latitude, longitude);
-    const coords = [latitude, longitude];
-    console.log(coords);
+// if (navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition((position) => {
+//     console.log(position);
+//     const { latitude } = position.coords;
+//     const { longitude } = position.coords;
+//     console.log(latitude, longitude);
+//     const coords = [latitude, longitude];
+//     console.log(coords);
 
-    const map = L.map("map").setView(coords, 13);
+//     const map = L.map("map").setView(coords, 13);
 
-    L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+//     L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+//       attribution:
+//         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//     }).addTo(map);
 
-    L.marker(coords)
-      .addTo(map)
-      .bindPopup("A pretty CSS popup.<br> Easily customizable.")
-      .openPopup();
+//     map.on("click", function (mapEvent) {
+//       console.log(mapEvent);
+//       const { lat, lng } = mapEvent.latlng;
 
-    map.on("click", function (mapEvent) {
-      console.log(mapEvent);
-    });
-  }),
-    function () {
-      alert("Could not get your location");
-    };
-}
+//       L.marker([lat, lng])
+//         .addTo(map)
+//         .bindPopup(
+//           L.popup({
+//             maxWidth: 250,
+//             minWidth: 100,
+//             autoClose: false,
+//             closeOnClick: false,
+//           })
+//         )
+//         .openPopup();
+//     });
+//   }),
+//     function () {
+//       alert("Could not get your location");
+//     };
+// }
+const searchInput = document.querySelector(".search__input");
+const searchIcon = document.querySelector(".icon__box");
 
-// const address = async function () {
-//   const data = await fetch(
-//     "https://geo.ipify.org/api/v2/country?apiKey=at_GYLwFZ0D18TfNHvx6hxDg9rF6tO11"
-//   );
-//   const res = await data.json();
-//   console.log(res);
-//   const ipAddress = document.querySelector(".ip-address");
-//   const location = document.querySelector(".location");
-//   const time = document.querySelector(".time");
-//   const isp = document.querySelector(".isp");
+let marker;
+let res;
+let map;
+const address = async function (ip = "") {
+  try {
+    const data = await fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=at_GYLwFZ0D18TfNHvx6hxDg9rF6tO11&ipAddress=${ip}`
+    );
+    res = await data.json();
+    console.log(res);
+    console.log(res.location);
+    const { lat } = res.location;
+    const { lng } = res.location;
+    const ipAddress = document.querySelector(".ip-address");
+    const location = document.querySelector(".location");
+    const country = document.querySelector(".country");
+    const time = document.querySelector(".time");
+    const isp = document.querySelector(".isp");
 
-//   ipAddress.textContent = res.ip;
-//   location.textContent = res.location.region;
-//   time.textContent = res.location.timezone;
-//   isp.textContent = res.isp;
-// };
-// address();
+    ipAddress.textContent = res.ip;
+    location.textContent = res.location.region;
+    country.textContent = res.location.country;
+    time.textContent = res.location.timezone;
+    isp.textContent = res.isp;
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          console.log(position);
+          const { latitude, longitude } = position.coords;
+          console.log(latitude, longitude);
+          const userCoords = [latitude, longitude];
+          const ipCoords = [lat, lng];
+          console.log(userCoords, ipCoords);
+
+          if (!map) {
+            map = L.map("map").setView(userCoords, 13);
+
+            L.tileLayer(
+              "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+              {
+                attribution:
+                  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+              }
+            ).addTo(map);
+          }
+
+          if (ipCoords[0] && ipCoords[1]) {
+            map.panTo(ipCoords);
+            L.marker(ipCoords)
+              .addTo(map)
+              .bindPopup(
+                L.popup({
+                  maxWidth: 250,
+                  minWidth: 100,
+                  autoClose: false,
+                  closeOnClick: false,
+                })
+              )
+              .setPopupContent(`This is your IP Address: ${res.ip}`)
+              .openPopup();
+          }
+        },
+        () => {
+          alert("Could not get your location");
+        }
+      );
+    }
+  } catch (error) {
+    alert(`Error : IP Address is invalid`);
+    console.log(res);
+    console.log(error);
+  }
+};
+
+address();
+
+searchIcon.addEventListener("click", function () {
+  console.log(searchInput.value);
+  address(searchInput.value);
+  searchInput.value = "";
+});
+
+document.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    console.log(searchInput.value);
+    address(searchInput.value);
+    searchInput.value = "";
+  }
+});
